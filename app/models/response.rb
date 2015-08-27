4ef4279a313d93aefaf39b27c8d3a1afa95dd34f
Connor Ring & Jasmine Feldmann & Jay Quigley & Matt Bogen <pair+ccring.jasmine-feldmann.jayqui.zebogen@devbootcamp.com>
2015-08-27 16:11:25 -0500

    Add associations and validations for response model

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,2 +1,6 @@
 class Response < ActiveRecord::Base
+  belongs_to :poll
+  belongs_to :question
+
+  validates_presence_of :poll_id, :question_id, :percentage, :answer, :created_at, :updated_at
 end
