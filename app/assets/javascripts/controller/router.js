74c81522925dce769884cba8438da2c91318b936
Jasmine Feldmann & Matt Bogen <pair+jasmine-feldmann.zebogen@devbootcamp.com>
2015-08-28 15:33:38 -0500

    Add results view and topics dropdown event to router

--- app/assets/javascripts/controller/router.js
+++ app/assets/javascripts/controller/router.js
@@ -1,18 +1,29 @@
 var AppRouter = Backbone.Router.extend({
   routes: {
-    'topics/:topic_id': 'showResults',
+    'topics/:topicId': 'showResults',
 
     '*actions': 'defaultAction'
   },
 
 
-  showResults: function(topic) {
-    var topicData;
+  showResults: function(topicId) {
+    var resultsView = new ResultsView();
+    resultsView.render();
+    var topicResponses = new TopicResponses([], { topicId: topicId });
+    topicResponses.fetch({
+      success: function(response) {
+        var questionsView = new QuestionsView({ collection: response.models });
+        questionsView.render();
+      }
+    });
   },
 
   defaultAction: function() {
     var homeView = new HomeView();
     homeView.render();
+    $('#topics-container').on('change', '#topics-dropdown', function(event) {
+      this.navigate("topics/" + $(event.target).val(), { trigger: true });
+    }.bind(this));
   }
 
 });
