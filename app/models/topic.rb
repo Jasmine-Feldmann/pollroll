77cc9528ae9815efd274d1dedf9399d5ffcac148
Connor Ring <email": null>
2015-08-28 11:30:45 -0500

    add responses method on topic model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,4 +4,11 @@ class Topic < ActiveRecord::Base
   has_many :polls, through: :responses
 
   validates_presence_of :name
+
+  def responses_json
+    topic_responses = []
+    self.questions.each { |q| topic_responses << { q => q.responses} }
+    topic_responses
+  end
+
 end
