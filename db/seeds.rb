6c382b009f2e5aaf48f7f69465ea1e3f61f8b966
Matt Bogen <zebogen@gmail.com>
2015-08-27 20:44:30 -0500

    Fix validation failure bug in seed logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,3 @@
-file = File.open("data-files/polls_all.json")
-file.readlines.each do |row|
-  json = JSON.parse(row)
-  json.each do |poll|
-    poll_obj = Poll.create!(build_poll_args(poll))
-    poll["questions"].each do |question|
-      topic = Topic.find_or_create_by(name: question["topic"] || question["name"])
-      question_obj = topic.questions.create!(build_question_args(question))
-      question["subpopulations"].each do |pop|
-        size = pop["observations"]
-        pop["responses"].each do |r|
-          Response.create!(build_response_args(r, poll_obj.id, question_obj.id))
-        end
-      end
-    end
-  end
-end
-
 def build_response_args(response, p_id, q_id)
   {
     percentage: response["value"],
@@ -27,7 +9,7 @@ end
 
 def build_question_args(question)
   {
-    state: question["state"],
+    state: question["state"] || "US",
     content: question["name"]
   }
 end
@@ -41,4 +23,30 @@ def build_poll_args(poll)
     partisan: poll["partisan"] != "Nonpartisan",
     affiliation: poll["affiliation"]
   }
+end
+
+def get_topic_name(question)
+  if question["topic"] == nil || question["topic"] == ""
+    question["name"]
+  else
+    question["topic"]
+  end
+end
+
+file = File.open("db/data-files/polls_all.json")
+file.readlines.each do |row|
+  json = JSON.parse(row)
+  json.each do |poll|
+    poll_obj = Poll.create!(build_poll_args(poll))
+    poll["questions"].each do |question|
+      topic = Topic.find_or_create_by!(name: get_topic_name(question))
+      question_obj = topic.questions.create!(build_question_args(question))
+      question["subpopulations"].each do |pop|
+        size = pop["observations"]
+        pop["responses"].each do |r|
+          Response.create!(build_response_args(r, poll_obj.id, question_obj.id))
+        end
+      end
+    end
+  end
 end
