82bfef6e4144ca7864f7575e27b2f379ac709823
Matt Bogen <zebogen@gmail.com>
2015-08-29 11:15:48 -0500

    Add bucketizing methods and call them in topic model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,8 +4,10 @@ class Topic < ActiveRecord::Base
 
   validates_presence_of :name
 
+  include ResponsesHelper
+
   def responses_json
-    self.charts.map {|c| { chart: c, responses: c.responses} }
+    self.charts.select { |ch| ch.responses.length > 0 }.map { |c| { chart: c, responses: bucketize_responses(c.responses) } }
   end
 
 end
