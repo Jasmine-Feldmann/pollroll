62857ad8eedbcdc9a9124cf821cd71ee1c9f27c5
Connor Ring & Matt Bogen <pair+ccring.zebogen@devbootcamp.com>
2015-08-29 14:14:11 -0500

    move filter logic to seed file

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -4,8 +4,4 @@ class Chart < ActiveRecord::Base
 
   validates_presence_of :topic_id, :name, :state, :slug
 
-  def filter_incomplete_polls
-    self.responses.order("date ASC").group_by(&:date).select { |dte, grp| grp.length == 3 }.map { |grp| grp[1] }.flatten
-  end
-
 end
