da363501b9ae94034fe7251f14451e911c632abc
Connor Ring & Matt Bogen <pair+ccring.zebogen@devbootcamp.com>
2015-08-29 14:51:06 -0500

    Format data on Rails side for Datamap, feed data in and render

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -6,8 +6,18 @@ class Topic < ActiveRecord::Base
 
   include ResponsesHelper
 
+  # def responses_json
+  #   self.charts.map { |c| { chart: c, responses: bucketize_responses(c.responses) } }
+  # end
+
   def responses_json
-    self.charts.map { |c| { chart: c, responses: bucketize_responses(c.responses) } }
+    keyed_json = {}
+    self.charts.each do |c|
+      sorted_responses = c.responses.order("date ASC").limit(3).sort_by { |r| r.percentage.to_f }
+      keyed_json[c.state] = { responses: sorted_responses,
+                              fillKey: sorted_responses.last.answer }
+    end
+    return keyed_json
   end
 
 end
