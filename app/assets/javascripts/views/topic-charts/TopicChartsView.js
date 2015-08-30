1295f1d438b86b003a3f5679039fb591c012b230
Jasmine Feldmann <email": null>
2015-08-30 12:02:42 -0500

    add functioning tabs that toggle between map view and chart view

--- app/assets/javascripts/views/topic-charts/TopicChartsView.js
+++ app/assets/javascripts/views/topic-charts/TopicChartsView.js
@@ -8,20 +8,26 @@ var TopicChartsView = Backbone.View.extend({
   render: function() {
     this.$el.html(this.template({ charts: this.collection }));
     drawDatamap(this.collection[0].attributes);
+    InitLineGraph(this.collection[0].attributes);
+    $("#line-graph-container").hide()
     var timeSlider = new TimeSlider();
     timeSlider.render();
     this.$el.find("#slider").on("slidechange", this.updateChartData.bind(this));
-    this.$el.find(".ui-tabs-nav").on("click", this.toggeleTab.bind(this))
+    this.$el.find(".ui-tabs-panel").on("click", this.toggleTab)
   },
 
   updateChartData: function(event, ui) {
     $("#map-container").html("");
     drawDatamap(this.collection[30 - ui.value].attributes);
-    InitLineGraph(this.collection[0].attributes);
   },
 
   toggleTab: function(event, ui) {
-    var active = $("ui-tabs").tabs("option", "active");
+    event.preventDefault();
+    $("div ul a li").removeClass("active");
+    $(".charts").hide()
+    var link = $(this.parentElement).attr("href")
+    $(this).addClass("active");
+    $(link).show();
   }
 
 })
