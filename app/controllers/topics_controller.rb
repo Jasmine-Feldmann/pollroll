b2a45bb55cd0fcb79d564a5bd7f15c5c0031a0bb
Connor Ring & Matt Bogen <pair+ccring.zebogen@devbootcamp.com>
2015-08-30 15:24:21 -0500

    Update topics controller to return the proper JSON response depending on topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,7 +7,12 @@ class TopicsController < ApplicationController
 
   def show
     @topic = Topic.find(params[:id])
-    render json: @topic.responses_json
+    case @topic.id
+    when 1
+      render json: @topic.responses_json_obama_approval
+    when 2
+      render json: @topic.responses_2016_gop
+    end
   end
 
 end
