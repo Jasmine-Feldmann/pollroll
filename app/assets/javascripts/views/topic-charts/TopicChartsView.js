628e2e98a9040083e441c2d9f5177b0a14e776de
Jasmine Feldmann <email": null>
2015-08-29 19:51:00 -0500

    Add beginning functionality for tabs showing only one tab

--- app/assets/javascripts/views/topic-charts/TopicChartsView.js
+++ app/assets/javascripts/views/topic-charts/TopicChartsView.js
@@ -11,12 +11,17 @@ var TopicChartsView = Backbone.View.extend({
     var timeSlider = new TimeSlider();
     timeSlider.render();
     this.$el.find("#slider").on("slidechange", this.updateChartData.bind(this));
+    this.$el.find(".ui-tabs-nav").on("click", this.toggeleTab.bind(this))
   },
 
   updateChartData: function(event, ui) {
     $("#map-container").html("");
     drawDatamap(this.collection[30 - ui.value].attributes);
     InitLineGraph(this.collection[0].attributes);
+  },
+
+  toggleTab: function(event, ui) {
+    var active = $("ui-tabs").tabs("option", "active");
   }
 
 })
