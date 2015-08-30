c7c6af9d5c1a39b5a8ced54bfec88a69be40b74e
Jay Quigley <quigley.james.g@gmail.com>
2015-08-29 22:05:26 -0500

    add text styling in tooltip

--- app/assets/javascripts/d3/map/NationalMap.js
+++ app/assets/javascripts/d3/map/NationalMap.js
@@ -1,9 +1,9 @@
 function toolTipHelper(dataSet, num) {
    return "<br>" 
-   + "<span class=data-answer>" 
+   + "<span class='data-answer'>" 
       + dataSet["responses"][num]["answer"] 
-   + "</span>" + " "
-   + "<span class=data-percentage>" 
+   + ":" + "</span>" + " "
+   + "<span class='data-percentage'>" 
       + dataSet["responses"][num]["percentage"] 
    + "</span>" + " "
    + "%"
@@ -11,37 +11,38 @@ function toolTipHelper(dataSet, num) {
 
 function drawDatamap(inputData){
    var map = new Datamap({
+      data: inputData,
       scope: 'usa',
       element: document.getElementById('map-container'),
       height: 700,
       geographyConfig: {
-        highlightBorderColor: '#bada55',
-      popupTemplate: function(geography, data) {
-         var abbr = STATEABBRS[geography.properties.name];
-         if (inputData[abbr]) {
-            return '<div class="hoverinfo">'
-            + geography.properties.name
-            + toolTipHelper(inputData[abbr], 0)
-            + toolTipHelper(inputData[abbr], 1)
-            + toolTipHelper(inputData[abbr], 2)
-            + "<br>sampleImage:" + "<img src='https://thingiverse-production-new.s3.amazonaws.com/renders/ed/21/ea/ac/8d/ray_graphics_thumb_tiny.jpg'>"
-         }
-         else {
-            return '<div class="hoverinfo">'
-            + geography.properties.name
-            + "<br>" + "Insufficient data"
-         }
+         highlightBorderColor: '#bada55',
+         highlightBorderWidth: 3,
+         popupTemplate: function(geography, data) {
+            var abbr = STATEABBRS[geography.properties.name];
+            if (inputData[abbr]) {
+               return '<div class="hoverinfo">'
+               + "<span class='state-name'>" + geography.properties.name + "</span>"
+               + toolTipHelper(inputData[abbr], 0)
+               + toolTipHelper(inputData[abbr], 1)
+               + toolTipHelper(inputData[abbr], 2)
+               + "<br>sampleImage:" + "<img src='https://thingiverse-production-new.s3.amazonaws.com/renders/ed/21/ea/ac/8d/ray_graphics_thumb_tiny.jpg'>"
+               + "</div>"
+            }
+            else {
+               return '<div class="hoverinfo">'
+               + "<span class='state-name'>" + geography.properties.name + "</span>"
+               + "<br>" + "Insufficient data"
+               + "</div>"
+            }
+         },
       },
-      highlightBorderWidth: 3
-      },
-
       fills: {
          'Disapprove': '#CC4731',
          'Approve': '#306596',
          'Undecided': 'purple',
          defaultFill: '#AAA'
       },
-      data: inputData
    });
 
    map.labels();
