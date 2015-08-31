f5d42afd01e49dd8f2118740851680f77891f80e
Connor Ring <email": null>
2015-08-31 09:45:38 -0500

    add method to seed topics from array of slugs, remove undecided filter from topic model

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,18 +55,41 @@ states.each do |state|
   end
 end
 
-gop_primary = Topic.create!(name: "2016 National GOP Primary")
-gop_slug = "2016-national-gop-primary"
+# gop_primary = Topic.create!(name: "2016 National GOP Primary")
+# gop_slug = "2016-national-gop-primary"
 
-gop_uri = URI(base_url + gop_slug)
-gop_response = Net::HTTP.get_response(gop_uri)
-gop_json = JSON.parse(gop_response.body)
-parse_chart_json(gop_json, gop_primary)
+# gop_uri = URI(base_url + gop_slug)
+# gop_response = Net::HTTP.get_response(gop_uri)
+# gop_json = JSON.parse(gop_response.body)
+# parse_chart_json(gop_json, gop_primary)
 
-dem_primary = Topic.create!(name: "2016 National Democratic Primary")
-dem_slug = "2016-national-democratic-primary"
+# dem_primary = Topic.create!(name: "2016 National Democratic Primary")
+# dem_slug = "2016-national-democratic-primary"
 
-dem_uri = URI(base_url + dem_slug)
-dem_response = Net::HTTP.get_response(dem_uri)
-dem_json = JSON.parse(dem_response.body)
-parse_chart_json(dem_json, dem_primary)
+# dem_uri = URI(base_url + dem_slug)
+# dem_response = Net::HTTP.get_response(dem_uri)
+# dem_json = JSON.parse(dem_response.body)
+# parse_chart_json(dem_json, dem_primary)
+
+# obama_foreign_policy = Topic.create!(name: "Obama Job Approval - Foreign Policy")
+# fp_slug = "obama-job-approval-foreign-policy"
+
+# fp_uri = URI(base_url + fp_slug)
+# fp_response = Net::HTTP.get_response(fp_uri)
+# fp_json = JSON.parse(fp_response.body)
+# parse_chart_json(fp_json, obama_foreign_policy)
+
+topic_array = [{name: "2016 National GOP Primary", slug: "2016-national-gop-primary"},
+               {name: "2016 National Democratic Primary", slug: "2016-national-democratic-primary"},
+               {name: "Obama Job Approval - Foreign Policy", slug: "obama-job-approval-foreign-policy"}]
+
+topic_array.each do |topic_hash|
+  new_topic = Topic.create!(name: topic_hash[:name])
+  slug = topic_hash[:slug]
+  base_url = "http://elections.huffingtonpost.com/pollster/api/charts/"
+  chart_uri = URI(base_url + slug)
+  chart_response = Net::HTTP.get_response(chart_uri)
+  chart_json = JSON.parse(chart_response.body)
+  parse_chart_json(chart_json, new_topic)
+end
+
