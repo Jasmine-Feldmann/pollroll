5e53aca10f4f0682dbeac431c0af60e31a4f15a5
Connor Ring & Jay Quigley <pair+ccring.jayqui@devbootcamp.com>
2015-08-31 15:20:39 -0500

    change color scale for line graphs

--- app/assets/javascripts/d3/lineGraph/choiceLineGraph.js
+++ app/assets/javascripts/d3/lineGraph/choiceLineGraph.js
@@ -4,6 +4,7 @@ function choiceLineGraph(nationalData) {
     choice.minDate = d3.min(choice.attributes.responses.map(function(d) { return parseDate.parse(d.date); }));
     choice.maxDate = d3.max(choice.attributes.responses.map(function(d) { return parseDate.parse(d.date); }));
     choice.maxPercentage = d3.max(choice.attributes.responses.map(function(d) { return d.percentage; }));
+    choice.name = choice.attributes.answer;
   });
   var graph = d3.select("#line-graph");
   var WIDTH = 1000;
@@ -25,7 +26,8 @@ function choiceLineGraph(nationalData) {
       d3.max(nationalData.map(function(choice) { return parseFloat(choice.maxPercentage) })) + 5
     ]);
 
-  var colorScale = d3.scale.category20();
+  var colorScale = d3.scale.ordinal()
+    .range(["#e41a1c","#377eb8","#4daf4a","#984ea3","#ff7f00","#a65628","#f781bf","#999999", "#66c2a5","#fc8d62","#8da0cb","#e78ac3","#a6d854","#ffd92f","#e5c494","#b3b3b3", "#8dd3c7","#bebada","#fb8072","#80b1d3","#fdb462","#b3de69","#fccde5","#d9d9d9","#bc80bd","#ccebc5", "#a6cee3","#1f78b4","#b2df8a","#33a02c","#fb9a99","#e31a1c","#fdbf6f","#ff7f00","#cab2d6","#6a3d9a","#b15928"]);
 
   var Xaxis = d3.svg.axis()
       .scale(Xscale)
@@ -58,6 +60,7 @@ function choiceLineGraph(nationalData) {
       .attr("stroke", colorScale(index))
       .attr("stroke-width", 3)
       .attr("fill", "none")
+      .attr("name", choice.name)
       .on("mouseover", mouseover)
       .on("mouseout", mouseout);
 
@@ -94,10 +97,22 @@ function choiceLineGraph(nationalData) {
     .style("stroke-width", 2);
 }
 
+// var focus = d3.select('svg')
+//   .append('g')
+//   .attr('class', 'focus')
+  // .style('display', 'none')
+
 function mouseover(d) {
   d3.select(this).classed("line-hover", true);
+  // console.log($(this).attr('name'));
+  // console.log(event.clientX, event.clientY);
+  // focus.append('text').text($(this).attr('name'))
+  //   .style('display', 'initial')
+  //   .attr('x', function(d) { return event.clientX; })
+  //   .attr('y', function(d) { return event.clientY; })
 }
 function mouseout(d) {
   d3.select(this).classed("line-hover", false);
+  // focus.style('display', 'none');
 }
 
