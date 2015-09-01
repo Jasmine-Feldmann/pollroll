a35f2b43c2aff1af643edc545f1e40adbd39a15b
Jay Quigley <quigley.james.g@gmail.com>
2015-09-01 10:58:27 -0500

    hide projections tab on all pages but the first

--- app/assets/javascripts/views/topic-charts/TopicChartsView.js
+++ app/assets/javascripts/views/topic-charts/TopicChartsView.js
@@ -26,6 +26,7 @@ var TopicChartsView = Backbone.View.extend({
       choiceLineGraph(this.collection);
       initBarChart(this.collection);
       $("#line-graph-container").hide();
+      $("#projections-tab").hide();
       this.$el.find(".ui-tabs-panel").on("click", this.toggleTab)
     }
   },
