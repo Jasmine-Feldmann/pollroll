351af5a296f2e175c7cfec25c1a8b993fa3b13c1
Matt Bogen <zebogen@gmail.com>
2015-09-03 13:20:37 -0500

    Remove unused charts and responses resources from routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-
   root 'pages#index'
   resources :topics, only: [:index, :show]
-  resources :charts, only: [:index, :show]
-  resources :responses, only: [:index, :show]
-
 end
